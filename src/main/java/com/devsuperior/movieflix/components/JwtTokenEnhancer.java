1cf882acc029d4a38cd1874faab86b36064c912e
Agny <agnyfonseca@outlook.com>
2022-01-19 09:53:03 +0100

    Repositories

--- src/main/java/com/devsuperior/movieflix/components/JwtTokenEnhancer.java
+++ src/main/java/com/devsuperior/movieflix/components/JwtTokenEnhancer.java
@@ -1,21 +1,29 @@
 package com.devsuperior.movieflix.components;
 
+import java.util.HashMap;
+import java.util.Map;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.oauth2.common.DefaultOAuth2AccessToken;
 import org.springframework.security.oauth2.common.OAuth2AccessToken;
 import org.springframework.security.oauth2.provider.OAuth2Authentication;
 import org.springframework.security.oauth2.provider.token.TokenEnhancer;
 import org.springframework.stereotype.Component;
 
+import com.devsuperior.movieflix.entities.User;
+import com.devsuperior.movieflix.repositories.UserRepository;
+
 
 @Component
 public class JwtTokenEnhancer implements TokenEnhancer {
 
-	/*@Autowired
-	private UserRepository userRepository; */
+	@Autowired
+	private UserRepository userRepository;
 	
 	@Override
 	public OAuth2AccessToken enhance(OAuth2AccessToken accessToken, OAuth2Authentication authentication) {
 
-		/*User user = userRepository.findByEmail(authentication.getName());
+		User user = userRepository.findByEmail(authentication.getName());
 		
 		Map<String, Object> map = new HashMap<>();
 		map.put("userId", user.getId());
@@ -23,8 +31,6 @@ public class JwtTokenEnhancer implements TokenEnhancer {
 		DefaultOAuth2AccessToken token = (DefaultOAuth2AccessToken) accessToken;
 		token.setAdditionalInformation(map);
 		
-		return accessToken; */
-		
-		return null;
+		return accessToken;
 	}
 }
