26d55781f0cc0f04ba18d002ac416ba1d8c81820
Agny Fonseca <agnyfonseca@outlook.com>
2023-01-07 14:06:22 +0000

    Movie new endpoints added

--- src/main/java/com/devsuperior/movieflix/controllers/MovieController.java
+++ src/main/java/com/devsuperior/movieflix/controllers/MovieController.java
@@ -1,21 +1,19 @@
 package com.devsuperior.movieflix.controllers;
 
-import java.util.List;
-
+import com.devsuperior.movieflix.dto.MovieDTO;
+import com.devsuperior.movieflix.dto.MovieDetailsDTO;
+import com.devsuperior.movieflix.dto.MovieInsertDTO;
+import com.devsuperior.movieflix.dto.ReviewDTO;
+import com.devsuperior.movieflix.services.MovieService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
-import com.devsuperior.movieflix.dto.MovieDTO;
-import com.devsuperior.movieflix.dto.MovieDetailsDTO;
-import com.devsuperior.movieflix.dto.ReviewDTO;
-import com.devsuperior.movieflix.services.MovieService;
+import java.net.URI;
+import java.util.List;
 
 @RestController
 @RequestMapping(value = "/movies")
@@ -23,15 +21,13 @@ public class MovieController {
 	
 	@Autowired
 	private MovieService service;
-	
-	//GET BY ID
+
 	@GetMapping(value = "/{id}")
 	public ResponseEntity<MovieDetailsDTO> findById(@PathVariable Long id) {
 		MovieDetailsDTO dto = service.findById(id);
 		return ResponseEntity.ok().body(dto);
 	}
-	
-	//GET MOVIES BY GENRE
+
 	@GetMapping
 	public ResponseEntity<Page<MovieDTO>> findByGenre(
 			@RequestParam(value = "genreId", defaultValue = "0") Long genreId,
@@ -40,11 +36,30 @@ public class MovieController {
 		Page<MovieDTO> page = service.findByGenre(genreId, pageable);
 		return ResponseEntity.ok().body(page);
 	}
-	
-	//GET MOVIE REVIEWS
+
 	@GetMapping(value = "/{movieId}/reviews")
 	public ResponseEntity<List<ReviewDTO>> findMovieReviews(@PathVariable Long movieId) {
 		List<ReviewDTO> list = service.findMovieReviews(movieId);
 		return ResponseEntity.ok().body(list);
+	}
+
+	@PostMapping
+	public ResponseEntity<MovieInsertDTO> insert(@RequestBody MovieInsertDTO dto) {
+		dto = service.insert(dto);
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}")
+				.buildAndExpand(dto.getId()).toUri();
+		return ResponseEntity.created(uri).body(dto);
+	}
+
+	@PutMapping(value = "/{id}")
+	public ResponseEntity<MovieInsertDTO> update(@PathVariable Long id, @RequestBody MovieInsertDTO dto) {
+		dto = service.update(id, dto);
+		return ResponseEntity.ok().body(dto);
+	}
+
+	@DeleteMapping(value = "/{id}")
+	public ResponseEntity<Void> delete(@PathVariable Long id) {
+		service.delete(id);
+		return ResponseEntity.noContent().build();
 	}
 }
--- src/main/java/com/devsuperior/movieflix/services/MovieService.java
+++ src/main/java/com/devsuperior/movieflix/services/MovieService.java
@@ -1,24 +1,28 @@
 package com.devsuperior.movieflix.services;
 
-import java.util.List;
-import java.util.Optional;
-import java.util.stream.Collectors;
-
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.Pageable;
-import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
-
 import com.devsuperior.movieflix.dto.MovieDTO;
 import com.devsuperior.movieflix.dto.MovieDetailsDTO;
+import com.devsuperior.movieflix.dto.MovieInsertDTO;
 import com.devsuperior.movieflix.dto.ReviewDTO;
 import com.devsuperior.movieflix.entities.Genre;
 import com.devsuperior.movieflix.entities.Movie;
 import com.devsuperior.movieflix.entities.Review;
 import com.devsuperior.movieflix.repositories.GenreRepository;
 import com.devsuperior.movieflix.repositories.MovieRepository;
+import com.devsuperior.movieflix.services.exceptions.DatabaseException;
 import com.devsuperior.movieflix.services.exceptions.ResourceNotFoundException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.dao.EmptyResultDataAccessException;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import javax.persistence.EntityNotFoundException;
+import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
 
 @Service
 public class MovieService {
@@ -53,5 +57,47 @@ public class MovieService {
 		List<Review> list = entity.getReviews();
 		
 		return list.stream().map(x -> new ReviewDTO(x)).collect(Collectors.toList()); 
+	}
+
+	@Transactional
+	public MovieInsertDTO insert(MovieInsertDTO dto) {
+		Movie entity = new Movie();
+		copyDtoToEntity(dto, entity);
+		entity = repository.save(entity);
+		return new MovieInsertDTO(entity);
+	}
+
+	@Transactional
+	public MovieInsertDTO update(Long id, MovieInsertDTO dto) {
+		try {
+			Movie entity = repository.getOne(id);
+			copyDtoToEntity(dto, entity);
+			entity = repository.save(entity);
+			return new MovieInsertDTO(entity);
+		}
+		catch (EntityNotFoundException e) {
+			throw new ResourceNotFoundException("Id not found " + id);
+		}
+	}
+
+	public void delete(Long id) {
+		try {
+			repository.deleteById(id);
+		}
+		catch (EmptyResultDataAccessException e) {
+			throw new ResourceNotFoundException("Id not found " + id);
+		}
+		catch (DataIntegrityViolationException e) {
+			throw new DatabaseException("Integrity violation");
+		}
+	}
+
+	private void copyDtoToEntity(MovieInsertDTO dto, Movie entity) {
+		entity.setTitle(dto.getTitle());
+		entity.setSubTitle(dto.getSubTitle());
+		entity.setYear(dto.getYear());
+		entity.setImgUrl(dto.getImgUrl());
+		entity.setSynopsis(dto.getSynopsis());
+		entity.setGenre(new Genre(dto.getGenreId(), null));
 	}
 }
